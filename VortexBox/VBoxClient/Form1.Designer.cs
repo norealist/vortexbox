42c87eeb633bf502b3a5687383a9f02ee66c797f
norealist <ivanlein777@gmail.com>
2025-10-11 11:15:45 +0300

    создал gui

--- VortexBox/VBoxClient/Form1.Designer.cs
+++ VortexBox/VBoxClient/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace VBoxClient
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listFiles = new ListBox();
+            buttonDownload = new Button();
+            buttonUpload = new Button();
+            fileInfo = new TextBox();
+            encryptFileOrNot = new CheckBox();
+            buttonDelete = new Button();
+            SuspendLayout();
+            // 
+            // listFiles
+            // 
+            listFiles.BackColor = SystemColors.InactiveBorder;
+            listFiles.BorderStyle = BorderStyle.FixedSingle;
+            listFiles.Font = new Font("YouTube Sans Light 48pt", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            listFiles.FormattingEnabled = true;
+            listFiles.Items.AddRange(new object[] { "...", "(DIR) Work", "forest.png", "video.mp4", "prog.exe", "secret.zip.enc" });
+            listFiles.Location = new Point(12, 12);
+            listFiles.Name = "listFiles";
+            listFiles.Size = new Size(400, 386);
+            listFiles.TabIndex = 0;
+            // 
+            // buttonDownload
+            // 
+            buttonDownload.Location = new Point(431, 269);
+            buttonDownload.Name = "buttonDownload";
+            buttonDownload.Size = new Size(262, 39);
+            buttonDownload.TabIndex = 1;
+            buttonDownload.Text = "Скачать";
+            buttonDownload.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpload
+            // 
+            buttonUpload.Location = new Point(431, 314);
+            buttonUpload.Name = "buttonUpload";
+            buttonUpload.Size = new Size(262, 39);
+            buttonUpload.TabIndex = 2;
+            buttonUpload.Text = "Загрузить";
+            buttonUpload.UseVisualStyleBackColor = true;
+            // 
+            // fileInfo
+            // 
+            fileInfo.BorderStyle = BorderStyle.FixedSingle;
+            fileInfo.Font = new Font("Cascadia Mono SemiLight", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            fileInfo.Location = new Point(431, 12);
+            fileInfo.Multiline = true;
+            fileInfo.Name = "fileInfo";
+            fileInfo.ReadOnly = true;
+            fileInfo.Size = new Size(262, 196);
+            fileInfo.TabIndex = 3;
+            fileInfo.Text = "### forest.png\r\n\r\nСоздан: 12.02.2029 13:51:10\r\nРазмер: 5 кб";
+            // 
+            // encryptFileOrNot
+            // 
+            encryptFileOrNot.AutoSize = true;
+            encryptFileOrNot.Location = new Point(431, 227);
+            encryptFileOrNot.Name = "encryptFileOrNot";
+            encryptFileOrNot.Size = new Size(182, 19);
+            encryptFileOrNot.TabIndex = 4;
+            encryptFileOrNot.Text = "Шифровать перед загрузкой";
+            encryptFileOrNot.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            buttonDelete.Location = new Point(431, 359);
+            buttonDelete.Name = "buttonDelete";
+            buttonDelete.Size = new Size(262, 39);
+            buttonDelete.TabIndex = 5;
+            buttonDelete.Text = "Удалить";
+            buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(712, 414);
+            Controls.Add(buttonDelete);
+            Controls.Add(encryptFileOrNot);
+            Controls.Add(fileInfo);
+            Controls.Add(buttonUpload);
+            Controls.Add(buttonDownload);
+            Controls.Add(listFiles);
+            Name = "MainWindow";
+            Text = "VortexBox";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listFiles;
+        private Button buttonDownload;
+        private Button buttonUpload;
+        private TextBox fileInfo;
+        private CheckBox encryptFileOrNot;
+        private Button buttonDelete;
     }
 }
--- VortexBox/VBoxClient/Form1.cs
+++ VortexBox/VBoxClient/Form1.cs
@@ -1,10 +1,15 @@
 namespace VBoxClient
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
